958b0f3285eed1a0993bc8089427498c8cefba7d
vintagehsieh <vintagehsieh@gmail.com>
2022-10-26 23:52:48 +0800

    加入專案檔案。

---
+++ Exec2_Stars/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Exec_Stars
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rowsLabel = new System.Windows.Forms.Label();
+			this.rowsTextBox = new System.Windows.Forms.TextBox();
+			this.leftButton = new System.Windows.Forms.Button();
+			this.rightButton = new System.Windows.Forms.Button();
+			this.isoTriangle = new System.Windows.Forms.Button();
+			this.resultTextBox = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// rowsLabel
+			// 
+			this.rowsLabel.AutoSize = true;
+			this.rowsLabel.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.rowsLabel.Location = new System.Drawing.Point(245, 56);
+			this.rowsLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.rowsLabel.Name = "rowsLabel";
+			this.rowsLabel.Size = new System.Drawing.Size(52, 25);
+			this.rowsLabel.TabIndex = 0;
+			this.rowsLabel.Text = "列數";
+			// 
+			// rowsTextBox
+			// 
+			this.rowsTextBox.Location = new System.Drawing.Point(300, 52);
+			this.rowsTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.rowsTextBox.MaxLength = 3;
+			this.rowsTextBox.Name = "rowsTextBox";
+			this.rowsTextBox.Size = new System.Drawing.Size(141, 25);
+			this.rowsTextBox.TabIndex = 1;
+			// 
+			// leftButton
+			// 
+			this.leftButton.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.leftButton.Location = new System.Drawing.Point(176, 100);
+			this.leftButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.leftButton.Name = "leftButton";
+			this.leftButton.Size = new System.Drawing.Size(115, 82);
+			this.leftButton.TabIndex = 2;
+			this.leftButton.Text = "產生\r\n靠左三角形";
+			this.leftButton.UseVisualStyleBackColor = true;
+			this.leftButton.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// rightButton
+			// 
+			this.rightButton.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.rightButton.Location = new System.Drawing.Point(300, 100);
+			this.rightButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.rightButton.Name = "rightButton";
+			this.rightButton.Size = new System.Drawing.Size(115, 82);
+			this.rightButton.TabIndex = 3;
+			this.rightButton.Text = "產生\r\n靠右三角形";
+			this.rightButton.UseVisualStyleBackColor = true;
+			this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+			// 
+			// isoTriangle
+			// 
+			this.isoTriangle.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+			this.isoTriangle.Location = new System.Drawing.Point(431, 100);
+			this.isoTriangle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.isoTriangle.Name = "isoTriangle";
+			this.isoTriangle.Size = new System.Drawing.Size(115, 82);
+			this.isoTriangle.TabIndex = 4;
+			this.isoTriangle.Text = "產生\r\n等腰三角形";
+			this.isoTriangle.UseVisualStyleBackColor = true;
+			this.isoTriangle.Click += new System.EventHandler(this.isoTriangle_Click);
+			// 
+			// resultTextBox
+			// 
+			this.resultTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.resultTextBox.Location = new System.Drawing.Point(176, 202);
+			this.resultTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.resultTextBox.Multiline = true;
+			this.resultTextBox.Name = "resultTextBox";
+			this.resultTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.resultTextBox.Size = new System.Drawing.Size(369, 413);
+			this.resultTextBox.TabIndex = 5;
+			this.resultTextBox.WordWrap = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(745, 665);
+			this.Controls.Add(this.resultTextBox);
+			this.Controls.Add(this.isoTriangle);
+			this.Controls.Add(this.rightButton);
+			this.Controls.Add(this.leftButton);
+			this.Controls.Add(this.rowsTextBox);
+			this.Controls.Add(this.rowsLabel);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.MinimumSize = new System.Drawing.Size(759, 700);
+			this.Name = "Form1";
+			this.Text = "三角形產生器";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label rowsLabel;
+		private System.Windows.Forms.TextBox rowsTextBox;
+		private System.Windows.Forms.Button leftButton;
+		private System.Windows.Forms.Button rightButton;
+		private System.Windows.Forms.Button isoTriangle;
+		private System.Windows.Forms.TextBox resultTextBox;
+	}
+}
+
---
+++ Exec2_Stars/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec_Stars
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int rows = 0;
+
+			try
+			{
+				rows = GetRows();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
+
+			//呼叫星星方法
+			string stars = GenerateLeftStars(rows);
+
+			//呈現星星
+			resultTextBox.Text = stars;
+		}
+
+		private void rightButton_Click(object sender, EventArgs e)
+		{
+			int rows = 0;
+
+			try
+			{
+				rows = GetRows();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+				return;
+			}
+
+			string stars = GenerateRightStars(rows);
+
+			resultTextBox.Text = stars;
+		}
+
+		private void isoTriangle_Click(object sender, EventArgs e)
+		{
+			int rows = 0;
+
+			try
+			{
+				rows = GetRows();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+
+			string stars = GenerateIsoStars(rows);
+
+			resultTextBox.Text = stars;
+		}
+
+		private string GenerateIsoStars(int rows)
+		{
+			string stars = string.Empty;
+
+			for (int i = 1; i <= rows; i++)
+			{
+				stars += new string(' ', (rows - i) * 2) + new string('*', 2 * i - 1) + "\r\n";
+			}
+
+			return stars;
+		}
+
+		private string GenerateRightStars(int rows)
+		{
+			string stars = string.Empty;
+
+			for (int i = 1; i <= rows; i++)
+			{
+				//stars += new string('*', i).PadLeft(rows, ' '*2) + "\r\n"; //看起來歪歪的
+				stars += new string(' ', (rows - i) * 2) + new string('*', i) + "\r\n";
+			}
+
+			return stars;
+		}
+
+		private string GenerateLeftStars(int rows)
+		{
+			string stars = string.Empty;
+
+			for (int i = 1; i <= rows; i++)
+			{
+				stars += new string('*', i) + "\r\n";
+			}
+
+			return stars;
+		}
+
+		private int GetRows()
+		{
+			//取得rowsTextBox中的數字
+			string input = rowsTextBox.Text;
+			bool isInt = int.TryParse(input, out int rows);
+
+			if (isInt == false)
+			{
+				throw new Exception("請輸入列數!");
+			}
+
+			if (rows < 0)
+			{
+				throw new Exception("請輸入非負的正整數!");
+			}
+
+			return rows;
+		}
+	}
+}
